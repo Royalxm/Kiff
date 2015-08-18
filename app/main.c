5c43ff8bd5189adee91fe975f5123dd5f5e800a2
Royall <mohamednachite@gmail.com>
2015-08-18 19:02:23 +0200

    Update main.c
    Fun

--- app/main.c
+++ app/main.c
@@ -11,11 +11,17 @@ static void activate (GtkApplication* app, gpointer user_data)
 
 int main(int argc, char **argv)
 {
+  
   GtkApplication *app;
   int status;
 
   app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  
+  
+  g_application_set_inactivity_timeout (app, 10000); // milliseconde
+
   status = g_application_run (G_APPLICATION (app), argc, argv);
   g_object_unref (kiff_window);
   
